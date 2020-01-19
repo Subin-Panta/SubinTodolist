17f42e48292c2bc227ff1297aa8bba3a7d5cb335
Subin-Panta <subinpanta9@gmail.com>
2020-01-19 16:05:05 +0545

    added component class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import TodoData from './data/TodoData'
 import ToDoitem from './components/ToDoItem'
-function App() {
-  const Data = TodoData.map(data => <ToDoitem key={data.id} item={data} />)
+//import { render } from '@testing-library/react'
+class App extends React.Component {
+  constructor()
+  {
+    super()
+    this.state={
+      todos: TodoData   
+    }
+  }
+  render(){
+  const Data = this.state.todos.map(data => <ToDoitem key={data.id} item={data} />)
   return (
    <div className="todo-list">
     {Data}
   </div>
   )
+  }
 }
 export default App
